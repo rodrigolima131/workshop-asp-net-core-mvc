6693effa6d82cb335a5191d7abc2b0a6d125171c
rodrigolima131 <rodrigo.lima131@gmail.com>
2019-07-12 14:44:48 -0300

    Firt controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace SalesWebMvc.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
-
+            ViewData["Email"] = "rodrigo.adelima.com";
             return View();
         }
 
