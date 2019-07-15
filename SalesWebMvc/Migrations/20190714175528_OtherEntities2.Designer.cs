316693471e774f669c679108a007900eb2c1f3f9
rodrigolima131 <rodrigo.lima131@gmail.com>
2019-07-15 13:03:21 -0300

    SellersController

--- SalesWebMvc/Migrations/20190714175528_OtherEntities2.Designer.cs
+++ SalesWebMvc/Migrations/20190714175528_OtherEntities2.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Migrations
 {
     [DbContext(typeof(SalesWebMvcContext))]
-    partial class SalesWebMvcContextModelSnapshot : ModelSnapshot
+    [Migration("20190714175528_OtherEntities2")]
+    partial class OtherEntities2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SalesWebMvc/Migrations/20190714175528_OtherEntities2.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class OtherEntities2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BaseSalary",
+                table: "SalesRecord");
+
+            migrationBuilder.RenameColumn(
+                name: "amount",
+                table: "SalesRecord",
+                newName: "Amount");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Seller",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Amount",
+                table: "SalesRecord",
+                newName: "amount");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Email",
+                table: "Seller",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "BaseSalary",
+                table: "SalesRecord",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
